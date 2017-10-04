f24abb7b98926b96538e04766e634293eb27444a
Wu Jian Ping <wujjpp@hotmail.com>
2017-10-04 22:24:20 +0800

    update packages

--- tools/config.js
+++ tools/config.js
@@ -7,7 +7,7 @@ import shared from '../settings'
 const config = {
   dev: {
     //这里publicPath没有直接使用`/`, 是因为在开发环境下我们需要依赖sourceMap, 具体说明可以查看：https://github.com/webpack-contrib/style-loader#recommended-configuration
-    publicPath: 'http://127.0.0.1:' + shared.frontPort + '/'
+    publicPath: 'http://localhost:' + shared.frontPort + '/'
   },
   sit: {
     //这里可以配置SIT环境CDN路径，打包完成后，将`/dist/public`目录复制到CDN，完成CDN加速
