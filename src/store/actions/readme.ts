83f5d23abba09a7d0a92f1c081af945ae33e21f2
Wu Jian Ping <jianping@bertadata.com>
2019-02-03 11:09:05 +0800

    enhance ts

--- src/store/actions/readme.ts
+++ src/store/actions/readme.ts
@@ -2,6 +2,7 @@
  * Created by Wu Jian Ping on 2017/2/8.
  */
 
+import { AxiosError, AxiosResponse } from "axios";
 import { Dispatch, Store } from "redux";
 import * as types from "../constants/readme";
 
@@ -12,13 +13,13 @@ export default function loadReadme() {
 
     return client
       .post("/api/loadReadme")
-      .then((res) => {
+      .then((res: AxiosResponse) => {
         dispatch({
           type: types.LOAD_README_SUCCESS,
           payload: res.data,
         });
       })
-      .catch((error) => {
+      .catch((error: AxiosError) => {
         dispatch({
           type: types.LOAD_README_ERROR,
           payload: error,
